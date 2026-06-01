4a887ef4fa1a86419c140785549281a3f99229fe
Anoop Kushwaha <186592607+Anoopk51@users.noreply.github.com>
2026-06-01 23:53:13 +0530

    Add: is-subsequence - submission-7

---
+++ Data Structures & Algorithms/is-subsequence/submission-7.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+         boolean found=false;
+         int match=0;
+        if(s.length()==0) return true;
+        if(s.length()>t.length()) return false;
+        for(int i=0;i<s.length();i++){
+            found=false;
+            for(int j=match;j<t.length();j++){
+                char sub=s.charAt(i);
+                char domain=t.charAt(j);
+                if(sub==domain){
+                    match=j+1;
+                    found=true;
+                    break;
+                }
+            }
+            if(!found){
+                return false;
+            }
+        }
+        return found;
+   
+    }
+}
