65dd80bcc4ff1c32c1c23789adc4e4d6972975db
Anoop Kushwaha <186592607+Anoopk51@users.noreply.github.com>
2026-06-03 13:06:16 +0530

    Add: kth-largest-element-in-an-array - submission-1

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-1.java
@@ -0,0 +1,77 @@
+class Solution {
+    // Approch-1 brute force
+    /*
+    public int findKthLargest(int[] nums, int k) {
+        int size=nums.length;
+        int startIndex=(size/2)-1;
+        for(int i=startIndex;i>=0;i--){
+            heapify(nums,i,size);
+        }
+
+        for(int i=size-1;i>0;i--){
+            swap(nums,0,i);
+            heapify(nums,0,i);
+        }
+        return nums[size-k];
+    }
+
+    void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+
+    void heapify(int[] nums,int i,int size){
+        int maxIndex=i;
+        int left=2*i+1;
+        int right=2*i+2;
+        if(left<size && nums[left]>nums[maxIndex]){
+            maxIndex=left;
+        }
+        if(right<size && nums[right]>nums[maxIndex]){
+            maxIndex=right;
+        }if(maxIndex!=i){
+            swap(nums,i,maxIndex);
+            heapify(nums,maxIndex,size);
+         }
+    }
+    */
+
+    // Approch-2
+
+    public int findKthLargest(int[] nums, int k) {
+        int size=nums.length;
+        int startIndex=(size/2)-1;
+        for(int i=startIndex;i>=0;i--){
+            heapify(nums,i,size);
+        }
+
+        for(int i=k-1;i>0;i--){
+            swap(nums,0,size-1);
+            heapify(nums,0,size-1);
+            size--;
+        }
+        return nums[0];
+    }
+
+    void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+
+    void heapify(int[] nums,int i,int size){
+        int maxIndex=i;
+        int left=2*i+1;
+        int right=2*i+2;
+        if(left<size && nums[left]>nums[maxIndex]){
+            maxIndex=left;
+        }
+        if(right<size && nums[right]>nums[maxIndex]){
+            maxIndex=right;
+        }if(maxIndex!=i){
+            swap(nums,i,maxIndex);
+            heapify(nums,maxIndex,size);
+         }
+    }
+}
