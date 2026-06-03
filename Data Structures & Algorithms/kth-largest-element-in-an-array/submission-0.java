5ccef7437b8992a6898370e6aef068b76972efa0
Anoop Kushwaha <186592607+Anoopk51@users.noreply.github.com>
2026-06-03 12:46:34 +0530

    Add: kth-largest-element-in-an-array - submission-0

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int size=nums.length;
+        int startIndex=(size/2)-1;
+        for(int i=startIndex;i>=0;i--){
+            heapify(nums,i,size);
+        }
+
+        for(int i=size-1;i>0;i--){
+            swap(nums,0,i);
+            heapify(nums,0,i);
+        }
+        return nums[size-k];
+    }
+
+    void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+
+    void heapify(int[] nums,int i,int size){
+        int maxIndex=i;
+        int left=2*i+1;
+        int right=2*i+2;
+        if(left<size && nums[left]>nums[maxIndex]){
+            maxIndex=left;
+        }
+        if(right<size && nums[right]>nums[maxIndex]){
+            maxIndex=right;
+        }if(maxIndex!=i){
+            swap(nums,i,maxIndex);
+            heapify(nums,maxIndex,size);
+         }
+    }
+}
