f16a555a3f61de88100a5542fc6b3fb2749c48eb
Anoop Kushwaha <186592607+Anoopk51@users.noreply.github.com>
2026-06-04 01:56:26 +0530

    Add: count-primes - submission-1

---
+++ Data Structures & Algorithms/count-primes/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countPrimes(int n) {
+        int count=0;
+        boolean[] prime=new boolean[n];
+        for(int i=2;i<n;i++){
+            prime[i]=true;
+        }
+
+        for(int i=2;i<Math.sqrt(n);i++){
+            if(prime[i]){
+                for(int j=i*i ;j<n; j+=i){
+                    prime[j]=false;
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            if(prime[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
